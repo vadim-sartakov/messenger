1d3522e204ec170debb99c6f0793ff30f2824eb7
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2020-04-04 16:50:06 +0500

    Add redux persist

--- web/src/createStore.js
+++ web/src/createStore.js
@@ -1,17 +1,27 @@
 import { createStore as reduxCreateStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import reducer from './reducers';
 import saga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-function createStore(preloadedState) {
-  const store = reduxCreateStore(reducer, preloadedState, composeEnhancers(
+const persistConfig = {
+  key: 'state',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+function createStore(preloadedState, persist) {
+  const store = reduxCreateStore(persist ? persistedReducer : reducer, preloadedState, composeEnhancers(
     applyMiddleware(sagaMiddleware)
   ));
+  const persistor = persistStore(store);
   sagaMiddleware.run(saga);  
-  return store;
+  return persist ? { store, persistor } : store;
 }
 
 export default createStore;
--- web/src/index.js
+++ web/src/index.js
@@ -4,21 +4,24 @@ import "regenerator-runtime/runtime";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import App from './components/App';
 import createStore from './createStore';
 import './index.css';
 
-const store = createStore();
+const { store, persistor } = createStore(undefined, true);
 const theme = createMuiTheme();
 
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <PersistGate persistor={persistor}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
