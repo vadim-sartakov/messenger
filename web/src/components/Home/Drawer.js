32a7a28815924cbe4ad669b549c3e1512cd09cd8
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2020-04-05 13:20:43 +0500

    Refactor drawer

---
+++ web/src/components/Home/Drawer.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import Hidden from '@material-ui/core/Hidden';
+import MuiDrawer from '@material-ui/core/Drawer';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/Inbox';
+import { makeStyles } from '@material-ui/core/styles';
+import { DRAWER_WIDTH } from './constants';
+
+const useStyles = makeStyles(theme => {
+  return {
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: DRAWER_WIDTH,
+        flexShrink: 0
+      }
+    },
+    drawerPaper: {
+      width: DRAWER_WIDTH,
+    },
+    avatar: {
+      marginRight: theme.spacing(2)
+    }
+  }
+});
+
+function getShortName(name) {
+  const parts = name.split(' ');
+  const string = parts.length === 2 ?
+    parts[0].charAt(0) + parts[1].charAt(0) :
+    name.substring(0, 2);
+  return string.toUpperCase();
+}
+
+function ResponsiveDrawer({ open, onClose, children }) {
+  const classes = useStyles();
+  return (
+    <nav className={classes.drawer}>
+      <Hidden smUp implementation="css">
+        <MuiDrawer
+          variant="temporary"
+          anchor="left"
+          open={open}
+          onClose={onClose}
+          classes={{ paper: classes.drawerPaper }}
+        >
+          {children}
+        </MuiDrawer>
+      </Hidden>
+      <Hidden xsDown implementation="css">
+        <MuiDrawer
+          variant="permanent"
+          classes={{ paper: classes.drawerPaper }}
+        >
+          {children}
+        </MuiDrawer>
+      </Hidden>
+    </nav>
+  )
+}
+
+function Drawer({ classes: rootClasses, user, open, onClose }) {
+  const classes = useStyles();
+  return (
+    <ResponsiveDrawer
+      open={open}
+      onClose={onClose}
+    >
+      <Grid
+        container
+        alignItems="center"
+        wrap="nowrap"
+        className={rootClasses.toolbar}
+      >
+        <Avatar
+          className={classes.avatar}
+          style={{
+            color: user.colors.text,
+            backgroundColor: user.colors.background
+          }}
+        >
+          {getShortName(user.name)}
+        </Avatar>
+        <Typography variant="h6" noWrap>
+          {user.name}
+        </Typography>
+      </Grid>
+      <Divider />
+      <List>
+        <ListSubheader>Chat rooms</ListSubheader>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map(text => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+        <ListSubheader>Friends</ListSubheader>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map(text => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </ResponsiveDrawer>
+  )
+}
+
+export default Drawer;
--- web/src/components/Home/Home.js
+++ web/src/components/Home/Home.js
@@ -3,22 +3,12 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Avatar from '@material-ui/core/Avatar';
 import ExitToApp from '@material-ui/icons/ExitToApp';
 import IconButton from '@material-ui/core/IconButton';
-import Hidden from '@material-ui/core/Hidden';
-import Drawer from '@material-ui/core/Drawer';
-import Divider from '@material-ui/core/Divider';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/Inbox';
 import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from './Drawer';
 import SessionExpiredDialog from './SessionExpiredDialog';
-
-const drawerWidth = 240;
+import { DRAWER_WIDTH } from './constants';
 
 const useStyles = makeStyles(theme => {
   return {
@@ -27,8 +17,8 @@ const useStyles = makeStyles(theme => {
     },
     appBar: {
       [theme.breakpoints.up('sm')]: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth
+        width: `calc(100% - ${DRAWER_WIDTH}px)`,
+        marginLeft: DRAWER_WIDTH
       }
     },
     menuButton: {
@@ -38,18 +28,6 @@ const useStyles = makeStyles(theme => {
     },
     title: {
       flexGrow: 1
-    },
-    drawer: {
-      [theme.breakpoints.up('sm')]: {
-        width: drawerWidth,
-        flexShrink: 0
-      }
-    },
-    drawerPaper: {
-      width: drawerWidth,
-    },
-    avatar: {
-      marginRight: theme.spacing(2)
     },
     content: {
       flexGrow: 1,
@@ -62,42 +40,7 @@ const useStyles = makeStyles(theme => {
   };
 });
 
-function ResponsiveDrawer({ open, onClose, children }) {
-  const classes = useStyles();
-  return (
-    <nav className={classes.drawer}>
-      <Hidden smUp implementation="css">
-        <Drawer
-          variant="temporary"
-          anchor="left"
-          open={open}
-          onClose={onClose}
-          classes={{ paper: classes.drawerPaper }}
-        >
-          {children}
-        </Drawer>
-      </Hidden>
-      <Hidden xsDown implementation="css">
-        <Drawer
-          variant="permanent"
-          classes={{ paper: classes.drawerPaper }}
-        >
-          {children}
-        </Drawer>
-      </Hidden>
-    </nav>
-  )
-}
-
-function getShortName(name) {
-  const parts = name.split(' ');
-  const string = parts.length === 2 ?
-    parts[0].charAt(0) + parts[1].charAt(0) :
-    name.substring(0, 2);
-  return string.toUpperCase();
-}
-
-function Home({ user, logout, chats }) {
+function Home({ user, logout }) {
   const classes = useStyles();
   const [openDrawer, setOpenDrawer] = useState(false);
   const toggleDrawer = () => setOpenDrawer(!openDrawer);
@@ -122,41 +65,12 @@ function Home({ user, logout, chats }) {
           </IconButton>
         </Toolbar>
       </AppBar>
-      <ResponsiveDrawer
+      <Drawer
+        classes={classes}
+        user={user}
         open={openDrawer}
         onClose={handleDrawerClose}
-      >
-        <Grid
-          container
-          alignItems="center"
-          wrap="nowrap"
-          className={classes.toolbar}
-        >
-          <Avatar
-            className={classes.avatar}
-            style={{
-              color: user.colors.text,
-              backgroundColor: user.colors.background
-            }}
-          >
-            {getShortName(user.name)}
-          </Avatar>
-          <Typography variant="h6" noWrap>
-            {user.name}
-          </Typography>
-        </Grid>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map(text => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </ResponsiveDrawer>
+      />
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <Typography paragraph>
