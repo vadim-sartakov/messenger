9507457024ebed5657d1fb45bd9932c6ab303b91
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2020-04-10 23:34:40 +0500

    Refactor LoginContainer

--- web/src/components/Login/LoginContainer.js
+++ web/src/components/Login/LoginContainer.js
@@ -1,22 +1,28 @@
-import React, { useCallback } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
 import { login } from '../../actions';
 import Login from './Login';
 
-function LoginContainer({ login, ...props }) {
+function LoginContainer({ token, login, ...props }) {
   const history = useHistory();
   const location = useLocation();
-  const onSubmit = useCallback(async credentials => {
-    const { from } = location.state || { from: { pathname: '/' } };
-    const onSuccess = () => history.replace(from);
-    login({ credentials, onSuccess });
-  }, [history, location.state, login]);
+  const { from } = location.state || { from: { pathname: '/' } };
+
+  useEffect(() => {
+    if (token) history.replace(from);
+  }, [token, from, history]);
+
+  const onSubmit = credentials => login({ credentials });
   return <Login {...props} onSubmit={onSubmit} />;
+}
+
+function mapStateToProps(state) {
+  return { token: state.auth.token };
 }
 
 function mapDispatchToProps(dispatch) {
   return { login: options => dispatch(login(options)) };
 }
 
-export default connect(undefined, mapDispatchToProps)(LoginContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer);
