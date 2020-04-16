7b932d5fac42765f7f5822cbac6f254a062e962c
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2020-04-17 01:54:25 +0500

    Update ws construction

--- web/src/constants.js
+++ web/src/constants.js
@@ -4,4 +4,4 @@ export const API_URL = process.env.API_URL || '';
 export const GRAPHQL_URL = process.env.GRAPHQL_URL || '';
 
 export const WS_URL = process.env.WS_URL ||
-  `${process.env.NODE_ENV === 'development' ? 'ws' : 'wss'}://${hostname}${port.length && ':' + port}/ws`;
+  `${process.env.NODE_ENV === 'development' ? 'ws' : 'wss'}://${hostname}${port.length ? ':' + port : ''}/ws`;
