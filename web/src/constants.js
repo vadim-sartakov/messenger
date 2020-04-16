a8928efa34dc28159155c5eba1bbf3d4d3739b67
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2020-04-17 01:49:13 +0500

    Set wss for production

--- web/src/constants.js
+++ web/src/constants.js
@@ -2,6 +2,6 @@ const { hostname, port } = window.location;
 
 export const API_URL = process.env.API_URL || '';
 export const GRAPHQL_URL = process.env.GRAPHQL_URL || '';
-// TODO: on production set 'wss'
+
 export const WS_URL = process.env.WS_URL ||
-  `${process.env.NODE_ENV === 'development' ? 'ws' : 'ws'}://${hostname}${port.length && ':' + port}/ws`;
+  `${process.env.NODE_ENV === 'development' ? 'ws' : 'wss'}://${hostname}${port.length && ':' + port}/ws`;
