a7a9ea0be06555de7cf747a60d3f0423fdc021c0
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2020-04-17 01:22:37 +0500

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
+git checkout -b build
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 "${DIR}/test.sh"
 "${DIR}/build.sh"
 cd "${DIR}/.."
+git add --all
+git commit -m "Update build"
 git subtree push --prefix server heroku master
+git checkout master
+git branch -D build
