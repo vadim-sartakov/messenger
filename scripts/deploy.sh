19e9a100d3a3a6d230302720c4bc6bf373a5d007
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2020-04-17 14:59:25 +0500

    Refactor scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd "${DIR}/.."
-heroku container:login
-heroku container:push web
-heroku container:release web
+"${DIR}"/test.sh &&\
+  heroku container:login &&\
+  heroku container:push web &&\
+  heroku container:release web
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,2 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd "${DIR}/../web"
-npm run test
-
-cd ../server
-npm run test
+cd "${DIR}/../web" && npm run test && cd ../server && npm run test
